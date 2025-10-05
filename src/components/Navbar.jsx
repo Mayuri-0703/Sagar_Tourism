401452f5beddc93dbc3d47db78f017d7b36f9331
Mayuri Pawar <mayurispawar03@gmail.com>
2025-10-05 22:44:16 +0530

    Updated Hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,65 @@
-// src/components/Navbar.jsx
-import './navbar.css';
-import logo from '../assets/Uday_Logo.jpg'
+// // src/components/Navbar.jsx
+// import './navbar.css';
+// import logo from '../assets/Uday_Logo.jpg'
+
+// function Navbar() {
+//   return (
+//     <header>
+//       {/* Top header: only logo */}
+//       <div className="top-header">
+//         <img src={logo} alt="Uday" className="logo" />
+//       </div>
+//       {/* Bottom header: navigation links */}
+//       <div className="bottom-header">
+//         <ul className="nav-links">
+//           <li><a href="#">Home</a></li>
+//           <li><a href="#">About</a></li>
+//           <li><a href="#">Destinations</a></li>
+//           <li><a href="#">Services</a></li>
+//           <li><a href="#">Contact</a></li>
+//         </ul>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Navbar;
+
+import { useState } from "react";
+import "./navbar.css";
+import logo from "../assets/uday_logo.jpg";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header>
-      {/* Top header: only logo */}
+      {/* Top Header */}
       <div className="top-header">
-        <img src={logo} alt="Uday" className="logo" />
+        <img src={logo} alt="Sagar Tourism Logo" className="logo" />
+        {/* Hamburger Icon */}
+        <div
+          className={`hamburger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      {/* Bottom header: navigation links */}
-      <div className="bottom-header">
+
+      {/* Bottom Header - Nav Links */}
+      <nav className={`bottom-header ${menuOpen ? "show" : ""}`}>
         <ul className="nav-links">
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
-          <li><a href="#">Destinations</a></li>
+          {/* <li><a href="#">Destinations</a></li> */}
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
-      </div>
+      </nav>
     </header>
   );
 }
 
 export default Navbar;
-
